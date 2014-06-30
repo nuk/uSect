a1073aa6dfdced2b8644d86a04707edc67199349
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2014-06-30 12:11:37 -0300

    Now carnivores chase hebivores

---
+++ test/org/unbiquitous/games/uSect/Sect_Behaviour_CarnivoreTest.java
@@ -0,0 +1,48 @@
+package org.unbiquitous.games.uSect;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import java.awt.Point;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.unbiquitous.uImpala.engine.core.GameComponents;
+import org.unbiquitous.uImpala.jse.impl.io.Screen;
+
+public class Sect_Behaviour_CarnivoreTest {
+	private Environment e;
+
+	@Before public void setUp(){
+		GameComponents.put(org.unbiquitous.uImpala.engine.io.Screen.class, new Screen());
+		e = new Environment();
+		e.random.setvalue(0);
+	}
+	
+	@Test public void goesAfterAHerbivoreAfterEachStep(){
+		Sect h = e.addSect(new Sect(new Point(20,20), new Herbivore()));
+		Sect c = e.addSect(new Sect(new Point(51,20), new Carnivore()));
+		
+		e.update();
+		
+		assertThat(h.center()).isEqualTo(new Point(20,20));
+		assertThat(c.center()).isEqualTo(new Point(50,20));
+	}
+	
+	@Test public void goesAfterAHerbivoreAfterEachStepEvenWhenHesMoving(){
+		e.addNutrient(new Point(50,20));
+		e.random.setvalue(-1);
+		Sect h = e.addSect(new Sect(new Point(10,20), new Herbivore()));
+		Sect c = e.addSect(new Sect(new Point(20,60), new Carnivore()));
+		
+		
+		for(int i = 0 ; i < 40; i ++){
+			e.update();
+		}
+		
+		
+		assertThat(h.center()).isEqualTo(new Point(50,20));
+		assertThat(c.center()).isNotEqualTo(new Point(20,20));
+	}
+	
+	//TODO: position must be asked from the environment
+}
