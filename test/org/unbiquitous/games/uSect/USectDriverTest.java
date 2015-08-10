2b5d88ee498b346a035e76c7c97c546e9dacc3d0
Luciano Santos <pdform@gmail.com>
2015-08-09 22:09:31 -0300

    Adapting to uos_core's replacement of JSON library.

--- test/org/unbiquitous/games/uSect/USectDriverTest.java
+++ test/org/unbiquitous/games/uSect/USectDriverTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.unbiquitous.games.uSect.TestUtils.setUpEnvironment;
 
+import java.util.Map;
 import java.util.UUID;
 
 import org.fest.assertions.data.MapEntry;
@@ -17,8 +18,8 @@ import org.junit.Test;
 import org.unbiquitous.games.uSect.environment.Environment;
 import org.unbiquitous.games.uSect.objects.Player;
 import org.unbiquitous.games.uSect.objects.Sect;
-import org.unbiquitous.uImpala.engine.core.GameSingletons;
 import org.unbiquitous.uImpala.engine.core.GameSettings;
+import org.unbiquitous.uImpala.engine.core.GameSingletons;
 import org.unbiquitous.uImpala.util.math.Point;
 import org.unbiquitous.uos.core.adaptabitilyEngine.Gateway;
 import org.unbiquitous.uos.core.applicationManager.CallContext;
@@ -26,71 +27,79 @@ import org.unbiquitous.uos.core.messageEngine.dataType.UpDevice;
 import org.unbiquitous.uos.core.messageEngine.messages.Call;
 import org.unbiquitous.uos.core.messageEngine.messages.Response;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class USectDriverTest {
+
+	private static final ObjectMapper mapper = new ObjectMapper();
 	
 	private GameSettings settings;
 	private Environment e;
 	private Gateway gateway;
-	
-	@Before public void setUp(){
+
+	@Before
+	public void setUp() {
 		gateway = mock(Gateway.class);
 		GameSingletons.put(Gateway.class, gateway);
 		settings = new GameSettings();
 		e = setUpEnvironment(settings);
 	}
-	
-	@Test public void respondToConnectWithPlayerId() {
+
+	@Test
+	public void respondToConnectWithPlayerId() {
 		UUID id = UUID.randomUUID();
 		settings.put("usect.player.id", id.toString());
 		e.addPlayer(new Player(id));
-		
+
 		USectDriver driver = new USectDriver(settings, e);
 		Response response = new Response();
 		driver.connect(null, response, mock(CallContext.class));
-		
-		assertThat(response.getResponseData()).hasSize(1).contains(
-				MapEntry.entry("player.id", id.toString()));
+
+		assertThat(response.getResponseData()).hasSize(1).contains(MapEntry.entry("player.id", id.toString()));
 	}
-	
-	@Test public void connectCallingDeviceWithPlayer() throws Exception{
+
+	@Test
+	public void connectCallingDeviceWithPlayer() throws Exception {
 		UUID id = UUID.randomUUID();
 		settings.put("usect.player.id", id.toString());
 		Player p = e.addPlayer(new Player(id));
-		
+
 		USectDriver driver = new USectDriver(settings, e);
-		CallContext ctx =  mock(CallContext.class);
+		CallContext ctx = mock(CallContext.class);
 		UpDevice target = new UpDevice("Target");
 		when(ctx.getCallerDevice()).thenReturn(target);
-		
+
 		driver.connect(null, new Response(), ctx);
 		p.call();
-		
+
 		verify(gateway).callService(eq(target), any(Call.class));
 	}
-	
-	@Test public void redirectCallToCorrespondingPlayer() {
+
+	@Test
+	public void redirectCallToCorrespondingPlayer() {
 		Player p1 = e.addPlayer(mock(Player.class));
 		Player p2 = e.addPlayer(mock(Player.class));
 		when(p2.id()).thenReturn(UUID.randomUUID());
 		Player p3 = e.addPlayer(mock(Player.class));
-		
+
 		USectDriver driver = new USectDriver(settings, e);
 		Call call = new Call();
 		call.addParameter("id", p2.id().toString());
 		driver.call(call, null, null);
-		
-		verify(p1,times(0)).call();
-		verify(p2,times(1)).call();
-		verify(p3,times(0)).call();
+
+		verify(p1, times(0)).call();
+		verify(p2, times(1)).call();
+		verify(p3, times(0)).call();
 	}
-	
-	@Test public void allowsTheMigrationOfSects() throws Exception {
+
+	@Test
+	public void allowsTheMigrationOfSects() throws Exception {
 		Sect s = setUpEnvironment().addSect(new Sect(), new Point());
 		USectDriver driver = new USectDriver(settings, e);
 		Call call = new Call();
-		call.addParameter("sect", s.toJSON().toMap());
+		call.addParameter("sect", mapper.treeToValue(s.toJSON(), Map.class));
 		driver.migrate(call, null, null);
-		
+
 		assertThat(e.sects()).containsOnly(s);
 	}
 }
